c3b06a1a2b87e82eaad4a93ed6a1a2e50ff46378
Helder Frans Guevara Acuña <helder26xd@gmail.com>
2023-05-26 11:51:00 -0500

    Encriptador terminado

--- js/encript.js
+++ js/encript.js
@@ -1,22 +1,75 @@
-function encryptText(){
-    var input=document.getElementById('inpText');
-    var output=document.getElementById('textAreaOut');
-
-    const message=input.value;
-    const encrymessage=encry(message);
-
-    output.value=encrymessage;
-}
-function encry(message){
-    const lowMessage= message.toLowerCase();
-    const encryMessage=lowMessage.replace(/[áéíóú]/g,'');
-    return encryMessage;
-}
-
 function copiar(){
-    var entrada=document.getElementById('textAreaOut');
+    var entrada=document.getElementById('outputText');
     const message=entrada.value;
     var output2=document.getElementById('inpText');
     output2.value=message;
-    return message;
 }
+function encryptText(encriptar) {
+    var inputText = document.getElementById("inpText").value;
+    var outputText = document.getElementById("outputText");
+  
+    if (encriptar) {
+      var encryptedText = encrypt(inputText);
+      outputText.value = encryptedText;
+    } else {
+      var decryptedText = decrypt(inputText);
+      outputText.value = decryptedText;
+    }
+  }
+  
+  function encrypt(text) {
+    var encryptedText = "";
+    for (var i = 0; i < text.length; i++) {
+      var character = text.charAt(i);
+      switch (character) {
+        case "a":
+          encryptedText += "ai";
+          break;
+        case "e":
+          encryptedText += "enter";
+          break;
+        case "i":
+          encryptedText += "imes";
+          break;
+        case "o":
+          encryptedText += "ober";
+          break;
+        case "u":
+          encryptedText += "ufat";
+          break;
+        default:
+          encryptedText += character;
+          break;
+      }
+    }
+    return encryptedText;
+  }
+  
+  function decrypt(text) {
+    var decryptedText = "";
+    var i = 0;
+    while (i < text.length) {
+      var character = text.charAt(i);
+      if (character === "a" && text.charAt(i + 1) === "i") {
+        decryptedText += "a";
+        i++;
+      } else if (character === "e" && text.substring(i, i + 5) === "enter") {
+        decryptedText += "e";
+        i += 4;
+      } else if (character === "i" && text.substring(i, i + 4) === "imes") {
+        decryptedText += "i";
+        i += 3;
+      } else if (character === "o" && text.substring(i, i + 4) === "ober") {
+        decryptedText += "o";
+        i += 3;
+      } else if (character === "u" && text.substring(i, i + 4) === "ufat") {
+        decryptedText += "u";
+        i += 3;
+      } else {
+        decryptedText += character;
+      }
+      i++;
+    }
+    return decryptedText;
+  }
+  
